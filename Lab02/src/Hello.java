bc5d32b367f73b23d6e36a24cca3e2e1514f27c7
mrazi <mrazi@DESKTOP-C3S2H93>
2021-03-12 13:42:28 -0800

    Added my name

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -7,7 +7,7 @@ public class Hello {
 		System.out.println("301940119 - Christopher Son.");
 		System.out.println("301151136 - Michael Mateo.");
 		System.out.println("302260218 - Haroon Mohammed Hussein.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("302285191 - Abdulaziz Ameri..");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
