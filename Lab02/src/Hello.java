a36e4f7604860098c9ebc97e77cb8d51607fa48c
husseiha <husseiha@L072846.calegis.net>
2021-03-12 12:56:40 -0800

    Third commit by Haroon

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -6,7 +6,7 @@ public class Hello {
 		// Add a new line with your information
 		System.out.println("301940119 - Christopher Son.");
 		System.out.println("301151136 - Michael Mateo.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("302260218 - Haroon Mohammed Hussein.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
