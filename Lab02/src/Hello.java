642162e2ba43ef0293f110086ddd484570299c29
John Cowgill <John Cowgill@10.0.0.61>
2021-03-12 16:25:13 -0800

    5th Commit

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -8,7 +8,7 @@ public class Hello {
 		System.out.println("301151136 - Michael Mateo.");
 		System.out.println("302260218 - Haroon Mohammed Hussein.");
 		System.out.println("302285191 - Abdulaziz Ameri.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("217799791 - John Cowgill.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
